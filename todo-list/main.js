1a40b21d565270ecb68234aa73c6ed903ed08b4e
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-04-29 10:18:01 +0900

    localStorageとアクセスする処理を追加

--- todo-list/main.js
+++ todo-list/main.js
@@ -1,3 +1,25 @@
+/* ローカルストレージ周りの実装
+ *
+ * Vue.jsの公式サンプルを参考にした
+ * https://jp.vuejs.org/v2/examples/todomvc.html
+ */
+var STORAGE_KEY = 'cr-vue-tutorial-todo-list'
+var todoStorage = {
+  fetch: function() {
+    var todos = JSON.parse(
+      localStorage.getItem(STORAGE_KEY) || '[]'
+    )
+    todos.forEach(function(todo, index) {
+      todo.id = index
+    })
+    todoStorage.uid = todos.length
+    return todos
+  },
+  save: function(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+}
+
 const app = new Vue({
   el: '#app',
   data: {
